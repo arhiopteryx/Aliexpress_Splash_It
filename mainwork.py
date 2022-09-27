f0f421b4e082dfbd27c4a7e58976a20ac959db46
arhiopteryx <43521959+Genya45@users.noreply.github.com>
2022-09-27 20:35:23 +0300

    Added new version
    One thread

---
+++ mainwork.py
@@ -0,0 +1,232 @@
+import shelve
+from time import sleep
+import random
+from urllib import request
+import requests
+import pandas as pd
+
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
+from bs4 import BeautifulSoup as bs
+
+LINK_SET_CONFIG = 'https://login.aliexpress.com/setCommonCookie.htm?currency=USD&region=UA&bLocale=en_US&site=glo&return=https://www.aliexpress.com'
+LINK_HELP_FRIEND = 'https://a.aliexpress.com/_ms4PVig'  # Link to Bring Down the Price
+LINK_MAIL = 'https://harakirimail.com/inbox/'
+
+MAIL_NAME = 'arhiopteryx'   # Enter a unique login to which a new account will be registered
+MAIL_NAME_CODE = '0c'   # Initial login uniqueness value
+MAIL_SECOND_NAME = '@harakirimail.com'  # Will be added to the login to generate mail
+PASSWORD_LABEL = 'genya1488'    #Enter the password for the created account
+
+#создаем объект браузера
+options = webdriver.ChromeOptions()
+
+mobileEmulation = {'deviceName': 'iPhone SE'}
+options.add_experimental_option('mobileEmulation', mobileEmulation)
+
+options.add_argument(f"user-agent=Mozilla/5.0 (Android 11; Mobile; rv:83.0) Gecko/83.0 Firefox/83.0")
+#options.add_argument(f'user-agent=Mozilla/5.0 (Linux; Android 4.1.1; GT-N7100 Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/35.0.1916.138 Mobile Safari/537.36 T7/6.3')
+options.add_argument('--allow-profiles-outside-user-dir')
+options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_experimental_option('useAutomationExtension', False)
+#options.set_headless(True)
+#browser = webdriver.Chrome(options=options)
+
+browser = webdriver.Chrome(options=options)
+browser.set_window_size(600, 600)
+
+#создаем объект браузера
+options = webdriver.ChromeOptions()
+options.add_argument(f"user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36")
+options.add_argument('--allow-profiles-outside-user-dir')
+options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_experimental_option('useAutomationExtension', False)
+#options.set_headless(True)
+browserMail = webdriver.Chrome(options=options)
+browserMail.set_window_size(600, 600)
+
+
+
+def funcRegOld():
+
+    #login
+    try:
+        sleep(1)
+        elem = browser.find_element(By.XPATH, '//*[@id="batman-dialog-wrap"]/div/div[2]/div[1]/div[2]/div/div/div/div[2]/span[1]/span[1]/input')                
+        elem.send_keys(MAIL_NAME + mailCodeName + MAIL_SECOND_NAME)
+    except:
+        print("error input login")
+        return
+
+    #password
+    try:        
+        elem = browser.find_element(By.XPATH, '//*[@id="batman-dialog-wrap"]/div/div[2]/div[1]/div[2]/div/div/div/div[3]/span/span[1]/input')
+        elem.send_keys(PASSWORD_LABEL)
+    except:
+        print("error click set password")
+
+    #click reg new user
+    try:        
+        elem = browser.find_element(By.CLASS_NAME, 'cosmos-btn')
+        elem.click()
+        sleep(1)
+    except:
+        print('error click enter login and password')
+        
+    
+    sleep(5)    #wait slide verification 
+    
+    try:     
+        #frame_0 = WebDriverWait(browser, 10).until(expected_conditions.presence_of_element_located((By.ID, "baxia-dialog-content")))
+
+        #to form slider
+        frame_0 = browser.find_element(By.ID, 'baxia-dialog-content')
+        browser.switch_to.frame(frame_0)
+        print('Поиск фрейма')
+
+        #find slider`s elements
+        slider = browser.find_element(By.ID, "nc_1_n1z")       
+        container = browser.find_element(By.ID, "nc_1__scale_text")   
+        
+        #try pull slider
+        move = ActionChains(browser)
+        move.click_and_hold(slider)
+        move.move_by_offset(int(container.size['width']-slider.size['width']), 0).release().perform()
+        
+
+        #exit form frame
+        browser.switch_to.default_content()  
+        #again click reg new user  
+        submit = browser.find_element(By.CLASS_NAME, 'cosmos-btn')
+        submit.click()
+        
+    except:
+        print("Slider non found")
+    
+    print("Get mail code")
+
+    sleep(1)
+    browserMail.get(LINK_MAIL + MAIL_NAME + mailCodeName)
+    elems = browserMail.find_elements_by_xpath("//a[@href]")
+    linkCurMail = ''
+    for elem in elems:
+        if 'email' in elem.get_attribute("href"):
+            linkCurMail = elem.get_attribute("href")
+            break
+
+    browserMail.get(linkCurMail)
+
+    textCode = browserMail.find_element(By.CLASS_NAME, 'code').text
+
+    print(f"code = {textCode}")
+
+
+    try:
+        sleep(2)        
+        elem = browser.find_element(By.XPATH, '//*[@id="batman-dialog-wrap"]/div/div[2]/div/div[2]/div/div/div/div[1]/input[1]')
+        elem.send_keys(textCode[0])
+        elem = browser.find_element(By.XPATH, '//*[@id="batman-dialog-wrap"]/div/div[2]/div/div[2]/div/div/div/div[1]/input[2]')
+        elem.send_keys(textCode[1])
+        elem = browser.find_element(By.XPATH, '//*[@id="batman-dialog-wrap"]/div/div[2]/div/div[2]/div/div/div/div[1]/input[3]')
+        elem.send_keys(textCode[2])
+        elem = browser.find_element(By.XPATH, '//*[@id="batman-dialog-wrap"]/div/div[2]/div/div[2]/div/div/div/div[1]/input[4]')
+        elem.send_keys(textCode[3])
+    except:
+        print("error set mail code")
+    try:        
+        elem = browser.find_element(By.CLASS_NAME, 'cosmos-btn')
+        elem.click()
+        sleep(1)
+    except:
+        print('error confirm code')
+
+    print("code input correct")
+    sleep(10)
+
+
+
+
+browser.get(LINK_SET_CONFIG)
+for i in range(300):
+    try:
+        mailCodeName = MAIL_NAME_CODE + str(i)
+        
+        browser.get('https://login.aliexpress.com/xman/xlogout.htm')
+        sleep(1)
+        browser.get(LINK_HELP_FRIEND)
+        sleep(2)
+
+        print(MAIL_NAME + mailCodeName + MAIL_SECOND_NAME + ':' + PASSWORD_LABEL + '----testing')
+
+        try:        
+            submit = browser.find_element(By.CLASS_NAME, 'rcButton--btnText--3M0lmD9')
+            submit.click()
+            sleep(5)
+        except:
+            print("error click help friend")
+
+        try:        
+            submit = browser.find_element(By.CLASS_NAME, 'rcButton--btnText--3M0lmD9')
+            submit.click()
+            sleep(5)
+        except:
+            print("error click help friend")
+
+        try:        
+            submit = browser.find_element(By.CLASS_NAME, 'scene-login-icon-more')
+            submit.click()
+            sleep(1)
+        except:
+            print("error click create new user")
+            #continue
+
+        
+        try:                    
+            sleep(2)
+            submit = browser.find_element(By.CLASS_NAME, 'cosmos-tabs-nav-item')
+            submit.click()
+            #funcRegNew()
+            funcRegOld()
+        except:
+            try:
+                print("error registration old")
+                funcRegOld()
+            except:
+                print("error registration new")
+                continue
+
+
+
+        try:        
+            submit = browser.find_element(By.CLASS_NAME, 'rcButton--btn--EMLV5lz')            
+            submit.click()
+            sleep(3)
+        except:
+            print("Click help Friend 1 error")
+        try:        
+            submit = browser.find_element(By.CLASS_NAME, 'rcButton--btn--EMLV5lz')            
+            submit.click()
+            sleep(3)
+        except:
+            print("Click help Friend 2 error")
+
+        sleep(5)
+        f = open('accounts.txt' , 'a')
+        f.write(MAIL_NAME + mailCodeName + MAIL_SECOND_NAME + ':' + PASSWORD_LABEL + '\n')
+        f.close()
+    
+    except:
+        print(MAIL_NAME + mailCodeName + MAIL_SECOND_NAME + ':' + PASSWORD_LABEL + 'error')
+        
+        
+        
